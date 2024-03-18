5172d24dea078c7bd5d87fda2dea8518e0ed2a76
Pawan Kumar <ratwanapawan@gmail.com>
2024-03-18 07:45:05 +0530

    Shuffle options

--- index.js
+++ index.js
@@ -10,29 +10,44 @@ const { correctAnswer, options, question } = questionObj;
 const questionEl = document.getElementById('question');
 const optionEl = document.getElementById('options');
 const scoreEl = document.getElementById('score');
+const nextBtn = document.getElementById('nextBtn');
+let score = 0;
+// Manipulate the DOM 
 
-
+// setting question text content 
 questionEl.textContent = question;
 
-options.forEach(option => {
+// Populating the Options div with the buttons 
+
+let shuffledOptions = shuffledOption(options);
+
+shuffledOptions.forEach(option => {
     const btn = document.createElement('button');
     btn.className = "btn";
     optionEl.appendChild(btn);
     btn.textContent = option;
-});
+    btn.addEventListener('click', () => {
+
+        if (option === correctAnswer) {
+            score = score + 1;
+        } else {
+            score = score - 0.25;
+        }
+        questionEl.textContent = "Quiz Completed!!"
+        optionEl.textContent = `Score: ${score}/5`
+        scoreEl.textContent = `<h2>Score: ${score}/5</h2>`;
+        nextBtn.remove()
+    });
+})
+
+// Shuffled Option
+
+function shuffledOption(options) {
+    for (let i = options.length - 1; i >= 0; i--) {
+        let j = Math.floor(Math.random() * 4);
 
 
-// options.map(option => {
-//     const optionBtn = document.createElement('button');
-//     optionBtn.className = "btn";
-//     optionEl.appendChild(optionBtn);
-//     optionBtn.textContent = option
-// })
-
-
-// for (let i = 0; i < options.length; i++) {
-//     const optionBtn = document.createElement('button');
-//     optionBtn.className = "btn";
-//     optionEl.appendChild(optionBtn);
-//     optionBtn.textContent = options[i];
-// }
+        [options[i], options[j]] = [options[j], options[i]];
+    }
+    return options;
+}
